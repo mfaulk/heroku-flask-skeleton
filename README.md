4fdb6097d28d20bcc7b2dfa2219ce59d4a4ac0e7
Matt Faulkner <mnfaulk@gmail.com>
2014-11-19 16:10:49 -0800

    Minimal flask app for heroku, based on https://github.com/yuvadm/heroku-python-skeleton

---
+++ README.md
@@ -0,0 +1,14 @@
+# Heroku Python Skeleton
+
+## Usage
+
+### Initial
+
+```bash
+$ git clone https://github.com/yuvadm/heroku-python-skeleton.git
+$ cd heroku-python-skeleton
+$ heroku create
+$ git push heroku master
+```
+
+### Database
---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/robots.txt')
+def robots():
+    res = app.make_response('User-agent: *\nAllow: /')
+    res.mimetype = 'text/plain'
+    return res
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
